9a86e4a60335fe482f2fe00314f38ac6cc922ed0
ALS.r59@gmail.com <ALS.r59@gmail.com@184b5982-6783-df22-9f1a-c0e4009c36d7>
2011-04-11 05:13:24 +0000

    Server Главный исполняемый файл для

---
+++ trunk/server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using System.Net;
+using System.Net.Sockets;
+
+using MySql.Data.MySqlClient;
+
+using System.Timers;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
