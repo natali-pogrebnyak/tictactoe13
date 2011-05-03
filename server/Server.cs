e2b6f49ec955cc3925d85659d115fd4be606d9c6
ALS.r59 <ALS.r59@gmail.com>
2011-05-03 04:16:06 +0000

    Server добавлена функция для обработки команд из командной строки

--- server/Server.cs
+++ server/Server.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Threading;
+
 namespace Server
 {
     class Server : IDisposable
@@ -17,6 +19,48 @@ namespace Server
         public void Start()
         {
             System.Console.WriteLine("Сервер \"" + config.vars["S_SERVER_NAME"] + "\" запущен!");
+            ReadCommand();
+        }
+
+        private void ReadCommand()
+        {
+            bool c_trg = true;
+            string str_command;
+            System.Console.Write("->>");
+            while (c_trg)
+            {
+                str_command = System.Console.ReadLine().ToString().ToLower().Trim();
+                switch (str_command)
+                {
+                    case "/exit":
+                        c_trg = false;
+                        System.Console.WriteLine("Команда " + str_command + " выполнена");
+                        break;
+                    case "/stop":
+                        c_trg = false;
+                        System.Console.WriteLine("Команда " + str_command + " выполнена");
+                        break;
+                    case "/restart":
+                        System.Console.WriteLine("Команда " + str_command + " не реализована!");
+                        break;
+                    case "/?":
+                        System.Console.WriteLine("Список команд сервера:");
+                        System.Console.WriteLine("/exit - остановка сервера");
+                        System.Console.WriteLine("/stop - остановка сервера");
+                        System.Console.WriteLine("/restart - перезапуск сервера");
+                        break;
+                    case "/help":
+                        System.Console.WriteLine("Список команд сервера:");
+                        System.Console.WriteLine("/exit - остановка сервера");
+                        System.Console.WriteLine("/stop - остановка сервера");
+                        System.Console.WriteLine("/restart - перезапуск сервера");
+                        break;
+                    default:
+                        System.Console.WriteLine("Команда не определена! воспользуйтесь командой /? или /help");
+                        break;
+                }
+                System.Console.Write("->>");
+            }
         }
 
         public void Dispose()
