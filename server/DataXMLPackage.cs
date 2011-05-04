c9d691abbeb461cbc0c63603bfe26e1321cb41f2
ALS.r59 <ALS.r59@gmail.com>
2011-05-04 08:40:11 +0000

    Server добавлен класс для работы с пакетами данных в формате XML с описанием всех полей

---
+++ server/DataXMLPackage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    [Serializable]
+
+    class DataXMLPackage
+    {
+        private string id_user; // идентификатор пользователя
+        private string recipient; // IP получатель
+        private string sender; // IP отправитель
+        private string type; // тип данных
+        private string data; // тип данных
+        private string date_s; // отправки
+        private string date_r; // дата приема
+        private string desc; // описание данных
+    }
+}
