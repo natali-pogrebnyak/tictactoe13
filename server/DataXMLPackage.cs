fe24b0621861ed0eda8ac4db159735f42a885eda
ALS.r59 <ALS.r59@gmail.com>
2011-05-05 07:55:10 +0000

    Server переписан класс DataXMLPackage + описание и xml ser

--- server/DataXMLPackage.cs
+++ server/DataXMLPackage.cs
@@ -2,20 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace Server
 {
     [Serializable]
-
     class DataXMLPackage
     {
-        private string id_user; // идентификатор пользователя
-        private string recipient; // IP получатель
-        private string sender; // IP отправитель
-        private string type; // тип данных
-        private string data; // тип данных
-        private string date_s; // отправки
-        private string date_r; // дата приема
-        private string desc; // описание данных
+        /// <summary>
+        /// идентификатор пользователя
+        /// </summary>
+        public string s_id_user
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// IP получатель
+        /// </summary>
+        public string s_ip_recipient
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// IP отправитель
+        /// </summary>
+        public string s_ip_sender
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// тип данных
+        /// </summary>
+        public string s_type
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// данные
+        /// </summary>
+        public string s_data
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// дата отправки
+        /// </summary>
+        public string d_date_s
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// дата приема
+        /// </summary>
+        [XmlIgnore]
+        public string d_date_r
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// описание данных
+        /// </summary>
+        public string s_desc
+        {
+            get;
+            set;
+        }
+
     }
 }
