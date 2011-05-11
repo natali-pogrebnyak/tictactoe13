6bbb3896c8df3bd7245675965e1df85c9c44ca0c
ALS.r59@gmail.com <ALS.r59@gmail.com@184b5982-6783-df22-9f1a-c0e4009c36d7>
2011-05-11 10:38:01 +0000

    Server добавлена функция остановки сервера а так же остановка сокета

--- trunk/server/DataExchange.cs
+++ trunk/server/DataExchange.cs
@@ -2,20 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+
+using System.Net;
+using System.Net.Sockets;
+
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Server
 {
     class DataExchange
     {
         private Server server;
+        private Socket mysocket;
+        private IPEndPoint sender;
+        static EndPoint Remote;
+        private DataXMLPackage client_command;
 
         public DataExchange(Server ob_server)
         {
             server = ob_server;
+            mysocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            mysocket.Bind(new IPEndPoint(IPAddress.Any, 9050));
+            sender = new IPEndPoint(IPAddress.Any, 0);
+            Remote = (EndPoint)(sender);
         }
 
-        public void dataConector () {
+        public void dataConector()
+        {
+            byte[] data_in;
+            int recv = 0;
 
+            while (true)
+            {
+                System.Console.WriteLine("asdasd");
+                data_in = new byte[1024];
+                recv = mysocket.ReceiveFrom(data_in, ref Remote);
+                XmlSerializer xmlFormat = new XmlSerializer(typeof(DataXMLPackage));
+                TextReader stringReader = new StringReader(Encoding.Default.GetString(data_in, 0, recv));
+                client_command = (DataXMLPackage)xmlFormat.Deserialize(stringReader);
+                client_command.d_date_r = String.Format("{0:dd.MM.yyyy HH:mm:ss}", DateTime.Now);
+                server.queue_command.Enqueue(client_command);
+            }
         }
+
+        public void Stop()
+        {
+            mysocket.Close();
+        }
+
     }
 }
--- trunk/server/Program.cs
+++ trunk/server/Program.cs
@@ -4,13 +4,6 @@ using System.Linq;
 using System.Text;
 using System.IO;
 
-using System.Net;
-using System.Net.Sockets;
-
-using MySql.Data.MySqlClient;
-
-using System.Timers;
-
 namespace Server
 {
     class Program
@@ -23,7 +16,6 @@ namespace Server
                 {
                     server.Start();
                 }
-            
             }
             catch (Exception ex)
             {
