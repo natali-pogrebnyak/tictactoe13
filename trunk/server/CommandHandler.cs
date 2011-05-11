4995f117316ce1a2e392d2d6fffa01a0eb14ffab
ALS.r59@gmail.com <ALS.r59@gmail.com@184b5982-6783-df22-9f1a-c0e4009c36d7>
2011-05-11 09:30:49 +0000

    Server Класс для обработки команд от консоли и от клиентов Из класса сервер изъята функция обработки команд и перенесена в класс CommandHandler В классе Server добавлен запуск потока для обработки команд от клиентов

---
+++ trunk/server/CommandHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    class CommandHandler
+    {
+        private Server server;
+        private string str_command;
+        private bool c_trg = true;
+
+        public CommandHandler(Server ob_server)
+        {
+            server = ob_server;
+        }
+
+        public void ReadCommandConsole()
+        {
+            bool c_trg = true;
+            string str_command;
+            System.Console.Write("->>");
+            while (c_trg)
+            {
+                str_command = System.Console.ReadLine().ToString().ToLower().Trim();
+                switch (str_command)
+                {
+                    case "/exit":
+                        c_trg = false;
+                        System.Console.WriteLine("Команда " + str_command + " выполнена");
+                        break;
+                    case "/stop":
+                        c_trg = false;
+                        System.Console.WriteLine("Команда " + str_command + " выполнена");
+                        break;
+                    case "/restart":
+                        System.Console.WriteLine("Команда " + str_command + " не реализована!");
+                        break;
+                    case "/?":
+                        System.Console.WriteLine("Список команд сервера:");
+                        System.Console.WriteLine("/exit - остановка сервера");
+                        System.Console.WriteLine("/stop - остановка сервера");
+                        System.Console.WriteLine("/restart - перезапуск сервера");
+                        break;
+                    case "/help":
+                        System.Console.WriteLine("Список команд сервера:");
+                        System.Console.WriteLine("/exit - остановка сервера");
+                        System.Console.WriteLine("/stop - остановка сервера");
+                        System.Console.WriteLine("/restart - перезапуск сервера");
+                        break;
+                    default:
+                        System.Console.WriteLine("Команда не определена! воспользуйтесь командой /? или /help");
+                        break;
+                }
+                System.Console.Write("->>");
+            }
+        }
+
+        public void ReadCommandClient()
+        {
+            bool c_trg = true;
+            string str_command = "";
+            while (c_trg)
+            {
+                switch (str_command)
+                {
+                    case "/exit":
+                        System.Console.WriteLine("Команда " + str_command + " выполнена");
+                        break;
+                    default:
+                        break;
+                }
+                System.Console.Write("->>");
+            }
+        }
+    }
+}
--- trunk/server/Server.cs
+++ trunk/server/Server.cs
@@ -11,65 +11,32 @@ namespace Server
     {
         private ConfigLoad config;
         private DataExchange data_exchange;
+        private CommandHandler command_handler;
+        private Thread thread_data;
         private Thread thread_command;
+        private Queue<DataXMLPackage> queue_command;
 
         public Server()
         {
             config = new ConfigLoad();
             data_exchange = new DataExchange(this);
-            this.thread_command = new Thread(this.data_exchange.dataConector);
-            this.thread_command.Start();
+            command_handler = new CommandHandler(this);
+
+            thread_data = new Thread(data_exchange.dataConector);// поток для приема и передачи данных
+            thread_data.Start();
+            thread_command = new Thread(command_handler.ReadCommandClient);// поток для обработки комманд клиента
+            thread_command.Start();
         }
 
         public void Start()
         {
             System.Console.WriteLine("Сервер \"" + config.vars["S_SERVER_NAME"] + "\" запущен!");
-            ReadCommand();
-        }
-
-        private void ReadCommand()
-        {
-            bool c_trg = true;
-            string str_command;
-            System.Console.Write("->>");
-            while (c_trg)
-            {
-                str_command = System.Console.ReadLine().ToString().ToLower().Trim();
-                switch (str_command)
-                {
-                    case "/exit":
-                        c_trg = false;
-                        System.Console.WriteLine("Команда " + str_command + " выполнена");
-                        break;
-                    case "/stop":
-                        c_trg = false;
-                        System.Console.WriteLine("Команда " + str_command + " выполнена");
-                        break;
-                    case "/restart":
-                        System.Console.WriteLine("Команда " + str_command + " не реализована!");
-                        break;
-                    case "/?":
-                        System.Console.WriteLine("Список команд сервера:");
-                        System.Console.WriteLine("/exit - остановка сервера");
-                        System.Console.WriteLine("/stop - остановка сервера");
-                        System.Console.WriteLine("/restart - перезапуск сервера");
-                        break;
-                    case "/help":
-                        System.Console.WriteLine("Список команд сервера:");
-                        System.Console.WriteLine("/exit - остановка сервера");
-                        System.Console.WriteLine("/stop - остановка сервера");
-                        System.Console.WriteLine("/restart - перезапуск сервера");
-                        break;
-                    default:
-                        System.Console.WriteLine("Команда не определена! воспользуйтесь командой /? или /help");
-                        break;
-                }
-                System.Console.Write("->>");
-            }
+            command_handler.ReadCommandConsole();
         }
 
         public void Dispose()
         {
+            thread_data.Abort();
             thread_command.Abort();
         }
     }
